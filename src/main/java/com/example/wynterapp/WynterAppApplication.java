d71f998bd0d35a649dfee1f72fed5b00e9f6f207
wynter <u0lee.dev@gmail.com>
2024-01-18 12:14:03 +0900

    init project

---
+++ src/main/java/com/example/wynterapp/WynterAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.wynterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WynterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WynterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wynterapp/WynterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wynterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WynterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
