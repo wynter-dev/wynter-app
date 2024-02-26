4c93f2d76541bbc75b083c9482cf523f492fe99b
wynter <u0lee.dev@gmail.com>
2024-02-26 11:27:57 +0900

    user 서비스 구조 잡기

--- src/main/java/com/example/wynterapp/WynterAppApplication.java
+++ src/main/java/com/example/wynterapp/WynterAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WynterAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WynterAppApplication.class, args);
     }
---
+++ src/main/java/com/example/wynterapp/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.wynterapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
